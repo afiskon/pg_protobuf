40d26d738a710481d78f2effb98832ef8e683f53
Aleksander Alekseev <afiskon@gmail.com>
2017-12-15 18:09:26 +0300

    A few more tests for various corner cases

--- sql/pg_protobuf.sql
+++ sql/pg_protobuf.sql
@@ -36,6 +36,10 @@ SELECT protobuf_get_string(E'\\x0a07616669736b6f6e10191880082a060a0200011064' ::
 
 SELECT protobuf_get_string(E'\\x10191880082a060a0200011064822007616669736b6f6e' :: bytea, 512);
 
+SELECT protobuf_get_string(E'\\x10321880022202100ffaa3e80303656178', 999999);
+
 SELECT protobuf_get_string(E'\\x10321880022202100f82200ad0a8d183d180d0b8d0ba' :: bytea, 512);
+
+SELECT length(protobuf_get_string(E'\\x10321880022202100f82208e02' || convert_to(repeat('a', 270), 'utf-8'), 512));
 
 DROP EXTENSION pg_protobuf;
