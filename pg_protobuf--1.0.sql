01341b0037273a8a61415ee0a7709ca5d0ccf590
Aleksander Alekseev <afiskon@gmail.com>
2018-01-16 13:59:24 +0300

    Tabs -> spaces

--- pg_protobuf--1.0.sql
+++ pg_protobuf--1.0.sql
@@ -38,37 +38,37 @@ CREATE FUNCTION protobuf_get_bytes(bytea, int)
 
 CREATE FUNCTION protobuf_get_string(data bytea, tag int) RETURNS text AS $$
 BEGIN
-	RETURN convert_from(protobuf_get_bytes(data, tag), 'utf-8');
+    RETURN convert_from(protobuf_get_bytes(data, tag), 'utf-8');
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
 CREATE FUNCTION protobuf_get_bool(data bytea, tag int) RETURNS boolean AS $$
 BEGIN
-	RETURN coalesce(protobuf_get_int(data, tag), 0) = 1;
+    RETURN coalesce(protobuf_get_int(data, tag), 0) = 1;
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
 CREATE FUNCTION protobuf_get_int(data bytea, tag int) RETURNS bigint AS $$
 BEGIN
-	RETURN (protobuf_get_int_multi(data, tag))[1];
+    RETURN (protobuf_get_int_multi(data, tag))[1];
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
 CREATE FUNCTION protobuf_get_sfixed32(data bytea, tag int) RETURNS int AS $$
 BEGIN
-	RETURN (protobuf_get_sfixed32_multi(data, tag))[1];
+    RETURN (protobuf_get_sfixed32_multi(data, tag))[1];
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
 CREATE FUNCTION protobuf_get_float(data bytea, tag int) RETURNS real AS $$
 BEGIN
-	RETURN (protobuf_get_float_multi(data, tag))[1];
+    RETURN (protobuf_get_float_multi(data, tag))[1];
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
 CREATE FUNCTION protobuf_get_sfixed64(data bytea, tag int) RETURNS bigint AS $$
 BEGIN
-	RETURN (protobuf_get_sfixed64_multi(data, tag))[1];
+    RETURN (protobuf_get_sfixed64_multi(data, tag))[1];
 END
 $$ LANGUAGE 'plpgsql' IMMUTABLE;
 
